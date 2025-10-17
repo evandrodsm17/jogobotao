a3a4a6928dc60f7b07ff0ce1ce97ce76853e07af
Evandro Mota <evandro.mota@labcmi.org.br>
2025-10-17 11:20:58 -0300

    Corrigido o erro do require

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocket = require("ws");
-const http = require = require("http");
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
